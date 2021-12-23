5b5be6a35372322f61476fd74761789a45dfb14a
Vlad M. Cranga <vladcranga@yahoo.com>
2021-12-24 01:45:03 +0200

    add .cs file

---
+++ TutorialFile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    class TutorialFile
+    {
+        static void Main(string[] args)
+        {
+            // write to the console
+            Console.WriteLine("Hello World");
+
+            // read from the console
+            //Console.ReadLine();
+
+            // strings
+            string characterName = "John";
+
+            // integers
+            int characterAge = 35;
+
+            // concatenation
+            Console.WriteLine("There was once a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+        }
+    }
+}
