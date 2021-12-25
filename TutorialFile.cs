33065d6c8c7d4d7f050bb184b2f68a55d8a370eb
Vlad M. Cranga <vladcranga@yahoo.com>
2021-12-25 11:28:10 +0200

    update .cs file

--- TutorialFile.cs
+++ TutorialFile.cs
@@ -25,6 +25,26 @@ namespace Tutorial
             // concatenation
             Console.WriteLine("There was once a man named " + characterName);
             Console.WriteLine("He was " + characterAge + " years old");
+
+            // data types
+            string phrase = "Giraffe Academy";
+            char grade = 'A';
+            int age = 30;
+            double gpa = 3.2; // float is less accurate, decimal is more accurate
+            bool isMale = true;
+
+            // working with strings
+            Console.WriteLine(phrase[0]);
+            Console.WriteLine(phrase.Length);
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.Contains("Academy"));
+            Console.WriteLine(phrase.Substring(8, 3));
+
+            // working with numbers
+            Console.WriteLine(5 * 8 + age);
+            Console.WriteLine(gpa - 1.1);
+            Console.WriteLine(Math.Pow(3, 2));
+
         }
     }
 }
